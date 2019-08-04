ca8a62cadce037318ae33c0d462d71f4b5fd14c9
heqingbao <qingbao.ho@gmail.com>
2019-08-04 13:42:03 +0800

    9-4 首页Model以及Dao

---
+++ lib/model/home_model.dart
@@ -0,0 +1,42 @@
+import 'package:flutter_trip/model/sales_box_model.dart';
+
+import 'common_model.dart';
+import 'config_model.dart';
+import 'grid_nav_model.dart';
+
+class HomeModel {
+  final ConfigModel config;
+  final List<CommonModel> bannerList;
+  final List<CommonModel> localNavList;
+  final List<CommonModel> subNavList;
+  final GridNavModel gridNav;
+  final SalesBoxModel salesBox;
+
+  HomeModel(
+      {this.config,
+      this.bannerList,
+      this.localNavList,
+      this.subNavList,
+      this.gridNav,
+      this.salesBox});
+
+  factory HomeModel.fromJson(Map<String, dynamic> json) {
+    var bannerListJson = json['bannerList'] as List;
+    List<CommonModel> bannerList =
+        bannerListJson.map((i) => CommonModel.fromJson(i)).toList();
+    var localNavListJson = json['localNavList'] as List;
+    List<CommonModel> localNavList =
+        localNavListJson.map((i) => CommonModel.fromJson(i)).toList();
+    var subNavListJson = json['subNavList'] as List;
+    List<CommonModel> subNavList =
+        subNavListJson.map((i) => CommonModel.fromJson(i)).toList();
+    return HomeModel(
+      config: ConfigModel.fromJson(json['config']),
+      bannerList: bannerList,
+      localNavList: localNavList,
+      subNavList: subNavList,
+      gridNav: GridNavModel.fromJson(json['gridNav']),
+      salesBox: SalesBoxModel.fromJson(json['salesBox']),
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:flutter_trip/dao/home_dao.dart';
+import 'package:flutter_trip/model/home_model.dart';
 
 const APPBAR_SCROLL_OFFSET = 100;
 
@@ -16,6 +20,13 @@ class _HomePageState extends State<HomePage> {
   ];
 
   double _appBarAlpha = 0;
+  String resultString = '';
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
 
   bool _onScroll(offset) {
     double alpha = offset / APPBAR_SCROLL_OFFSET;
@@ -28,6 +39,19 @@ class _HomePageState extends State<HomePage> {
       _appBarAlpha = alpha;
     });
     return true;
+  }
+
+  loadData() async {
+//    try {
+    HomeModel model = await HomeDao.fetch();
+    setState(() {
+      resultString = json.encode(model.config);
+    });
+//    } catch (e) {
+//      setState(() {
+//        resultString = e.toString();
+//      });
+//    }
   }
 
   @override
@@ -65,7 +89,7 @@ class _HomePageState extends State<HomePage> {
                     Container(
                       height: 800,
                       child: ListTile(
-                        title: Text("list"),
+                        title: Text(resultString),
                       ),
                     )
                   ],
