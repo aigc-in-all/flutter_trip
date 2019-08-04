7cb8a795f6b9001af7c258d6138d3814bbf26efd
heqingbao <qingbao.ho@gmail.com>
2019-08-04 17:30:47 +0800

    9-11 首页Loading & 下拉刷新

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,9 +6,11 @@ import 'package:flutter_trip/model/grid_nav_model.dart';
 import 'package:flutter_trip/model/home_model.dart';
 import 'package:flutter_trip/model/sales_box_model.dart';
 import 'package:flutter_trip/widgets/grid_nav.dart';
+import 'package:flutter_trip/widgets/loading_container.dart';
 import 'package:flutter_trip/widgets/local_nav.dart';
 import 'package:flutter_trip/widgets/sales_box.dart';
 import 'package:flutter_trip/widgets/sub_nav.dart';
+import 'package:flutter_trip/widgets/webview.dart';
 
 const APPBAR_SCROLL_OFFSET = 100;
 
@@ -18,22 +20,19 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List _imageUrls = [
-    'http://pages.ctrip.com/commerce/promote/20180718/yxzy/img/640sygd.jpg',
-    'https://dimg04.c-ctrip.com/images/700u0r000000gxvb93E54_810_235_85.jpg',
-    'https://dimg04.c-ctrip.com/images/700c10000000pdili7D8B_780_235_57.jpg'
-  ];
-
   double _appBarAlpha = 0;
+  List<CommonModel> bannerList = [];
   List<CommonModel> localNavList = [];
   GridNavModel gridNavModel;
   List<CommonModel> subNavList = [];
   SalesBoxModel salesBoxModel;
 
+  bool _loading = true;
+
   @override
   void initState() {
     super.initState();
-    loadData();
+    _handleRefresh();
   }
 
   bool _onScroll(offset) {
@@ -49,94 +48,125 @@ class _HomePageState extends State<HomePage> {
     return true;
   }
 
-  loadData() async {
+  Future<Null> _handleRefresh() async {
+    print('refresh');
     try {
       HomeModel model = await HomeDao.fetch();
       setState(() {
+        bannerList = model.bannerList;
         localNavList = model.localNavList;
         gridNavModel = model.gridNav;
         subNavList = model.subNavList;
         salesBoxModel = model.salesBox;
+        _loading = false;
       });
     } catch (e) {
       print(e);
+      setState(() {
+        _loading = false;
+      });
     }
+    return null;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xfff2f2f2),
-      body: Stack(
-        children: <Widget>[
-          MediaQuery.removePadding(
-            // 移除ListView距离屏幕顶部的边距
-              removeTop: true,
-              context: context,
-              child: NotificationListener(
-                onNotification: (notification) {
-                  if (notification is ScrollUpdateNotification &&
-                      notification.depth ==
-                          0 /*只有ListView滚动的时候才处理，否则swiper滚动也会调用*/) {
-                    return _onScroll(notification.metrics.pixels);
-                  }
-                  return false;
-                },
-                child: ListView(
-                  children: <Widget>[
-                    Container(
-                      height: 160,
-                      child: Swiper(
-                        itemCount: 3,
-                        autoplay: true,
-                        itemBuilder: (context, index) {
-                          return Image.network(_imageUrls[index],
-                              fit: BoxFit.fill);
-                        },
-                        pagination: SwiperPagination(),
+      body: LoadingContainer(
+          isLoading: _loading,
+          child: Stack(
+            children: <Widget>[
+              MediaQuery.removePadding(
+                // 移除ListView距离屏幕顶部的边距
+                removeTop: true,
+                context: context,
+                child: NotificationListener(
+                  onNotification: (notification) {
+                    if (notification is ScrollUpdateNotification &&
+                        notification.depth ==
+                            0 /*只有ListView滚动的时候才处理，否则swiper滚动也会调用*/) {
+                      return _onScroll(notification.metrics.pixels);
+                    }
+                    return false;
+                  },
+                  child: RefreshIndicator(
+                      child: ListView(
+                        children: <Widget>[
+                          Container(
+                            height: 160,
+                            child: Swiper(
+                              itemCount: bannerList.length,
+                              autoplay: true,
+                              itemBuilder: (context, index) {
+                                return GestureDetector(
+                                  onTap: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) {
+                                          CommonModel model = bannerList[index];
+                                          return WebView(
+                                            url: model.url,
+                                            statusBarColor:
+                                            model.statusBarColor,
+                                            hideAppBar: model.hideAppBar,
+                                          );
+                                        },
+                                      ),
+                                    );
+                                  },
+                                  child: Image.network(
+                                    bannerList[index].icon,
+                                    fit: BoxFit.fill,
+                                  ),
+                                );
+                              },
+                              pagination: SwiperPagination(),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(7, 4, 7, 4),
+                            child: LocalNav(localNavList: localNavList),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(7, 0, 7, 4),
+                            child: GridNav(gridNavModel: gridNavModel),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(7, 0, 7, 4),
+                            child: SubNav(subNavList: subNavList),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(7, 0, 7, 4),
+                            child: SalesBox(salesBox: salesBoxModel),
+                          ),
+                          Container(
+                            height: 800,
+                            child: ListTile(
+                              title: Text("haha"),
+                            ),
+                          )
+                        ],
                       ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(7, 4, 7, 4),
-                      child: LocalNav(localNavList: localNavList),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(7, 0, 7, 4),
-                      child: GridNav(gridNavModel: gridNavModel),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(7, 0, 7, 4),
-                      child: SubNav(subNavList: subNavList),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(7, 0, 7, 4),
-                      child: SalesBox(salesBox: salesBoxModel),
-                    ),
-
-                    Container(
-                      height: 800,
-                      child: ListTile(
-                        title: Text("haha"),
-                      ),
-                    )
-                  ],
-                ),
-              )),
-          Opacity(
-            opacity: _appBarAlpha,
-            child: Container(
-              height: 80,
-              decoration: BoxDecoration(color: Colors.white),
-              child: Center(
-                child: Padding(
-                  padding: EdgeInsets.only(top: 20),
-                  child: Text('首页'),
+                      onRefresh: _handleRefresh),
                 ),
               ),
-            ),
-          )
-        ],
-      ),
+              Opacity(
+                opacity: _appBarAlpha,
+                child: Container(
+                  height: 80,
+                  decoration: BoxDecoration(color: Colors.white),
+                  child: Center(
+                    child: Padding(
+                      padding: EdgeInsets.only(top: 20),
+                      child: Text('首页'),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          )),
     );
   }
 }
