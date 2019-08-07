a37042fc523ff5a714dcf7e487f3c086b6f006ea
heqingbao <qingbao.ho@gmail.com>
2019-08-08 00:46:36 +0800

    搜索页面 Flutter Plugin开发 —— Android端实现

--- android/app/src/main/java/com/flutter/flutter_trip/MainActivity.java
+++ android/app/src/main/java/com/flutter/flutter_trip/MainActivity.java
@@ -2,6 +2,8 @@ package com.flutter.flutter_trip;
 
 import android.os.Bundle;
 
+import com.demo.flutter.plugin.asr.AsrPlugin;
+
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
@@ -10,5 +12,10 @@ public class MainActivity extends FlutterActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         GeneratedPluginRegistrant.registerWith(this);
+        registerSelfPlugin();
+    }
+
+    private void registerSelfPlugin() {
+        AsrPlugin.registerWith(registrarFor("com.demo.flutter.plugin.asr.AsrPlugin"));
     }
 }
